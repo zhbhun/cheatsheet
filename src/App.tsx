4a9192e607f458d4e1838d58033be24df3561c7e
zhbhun <zhbhun@gmail.com>
2024-09-06 15:11:42 +0800

    feat: add yaml parser

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import * as yaml from 'yaml';
 
 function App() {
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    import('@/data/typescript/index.yaml?raw').then(({ default: content }) => {
+      console.log(content);
+      console.log(yaml.parse(content));
+    });
+  }, []);
 
   return (
     <>
-      <button className="text-red-500" onClick={() => setCount((count) => count + 1)}>
+      <button
+        className="text-red-500"
+        onClick={() => setCount((count) => count + 1)}
+      >
         count is {count}
       </button>
     </>
