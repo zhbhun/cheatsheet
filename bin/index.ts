0c94336400c624e6dde329e6a14709cee39cc8fc
zhbhun <zhbhun@gmail.com>
2025-04-15 15:45:46 +0800

    feat: adjust feature generate script

--- bin/index.ts
+++ bin/index.ts
@@ -1,3 +1,9 @@
+import process from 'node:process';
 import { generateFeature } from './feature.ts';
 
-generateFeature('swift', 'gui/view/custom').catch(console.error);
+const language = process.argv[2]; // swift
+const feature = process.argv[3]; // gui/view/custom
+
+if (feature) {
+  generateFeature(language, feature).catch(console.error);
+}
